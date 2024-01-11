c8be57f87b55facf11e4e8d572e5092f9c6079a3
cjdemille <cjdemille5@gmail.com>
2024-01-11 17:07:08 -0500

    create static page

---
+++ README.md
@@ -0,0 +1,1 @@
+Eat-n-Split project as part of the Jonas Schmedtmann Ultimate React Course 
---
+++ src/App.jsx
@@ -0,0 +1,115 @@
+import { useState } from 'react'
+
+
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+
+
+function App() {
+
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
+      </div>
+      <FormSplitBill />
+
+    </div>
+  )
+}
+
+export default App
+
+
+
+function FriendsList() {
+
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (<Friend friend={friend} key={friend.id} />)
+
+      )}
+    </ul>
+  )
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && <p className="red">You owe {friend.name} ${Math.abs(friend.balance)} </p>}
+      {friend.balance > 0 && <p className="green">{friend.name} owes you ${Math.abs(friend.balance)} </p>}
+      {friend.balance === 0 && <p>You and {friend.name} are even. </p>}
+      <Button className="button">Select</Button>
+    </li>
+  )
+}
+
+function FormAddFriend() {
+  return (
+    <form action="#" className="form-add-friend">
+      <label>👯 Friend Name  </label>
+      <input type="text" />
+
+      <label>🌆 Image Url</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  )
+}
+
+function Button({ children }) {
+  return (
+    <button className="button">{children}</button>
+  )
+}
+
+function FormSplitBill() {
+  return (
+    <form action="#" className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>💵 Bill Value  </label>
+      <input type="text" />
+
+      <label>💰 Your expense</label>
+      <input type="text" />
+
+      <label>💰 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+      <select name="" id="">
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
+
+    </form>
+  )
+}
